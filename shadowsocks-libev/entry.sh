42b828073b3d12dc498b2ebc3ad6d3547d3be5c3
Wang Guan <momocraft@gmail.com>
2016-07-10 23:09:53 +0900

    minor fix - use -a to run as nobody - merge startup scripts

--- shadowsocks-libev/entry.sh
+++ shadowsocks-libev/entry.sh
@@ -4,17 +4,29 @@ set -ue
 
 failed_dropping_lan=false
 for private_range in "10.0.0.0/8" "172.16.0.0/12" "192.168/16" "fd00::/8" ; do
-  if ip route add blackhole "$private_range"; then
-    echo "INFO: dropped connection to $private_range"
+  if ip route add prohibit "$private_range"; then
+    echo "INFO: prohibited connection to $private_range"
   else
-    echo "WARNING: failed dropping connection with 'ip route add blackhole $private_range'."
+    echo "WARNING: failed prohibiting connection to IP range '$private_range'."
     failed_dropping_lan=true
   fi
 done
 
 if [[ "$failed_dropping_lan" != "false" ]]; then
-  echo "This SECURITY RISK may allow shadowsocks client to access your private LAN."
-  echo "You may want to start with --cap-add=NET_ADMIN or in host"
+  echo "This SECURITY RISK may expose your private LAN to shadowsocks clients."
+  echo "You are advised to \`docker run --cap-add=NET_ADMIN\` to prevent this."
 fi
 
-exec su nobody -s /bin/sh -c "sh /run.sh"
+echo "INFO: SERVER_ADDR=$SERVER_ADDR"
+echo "INFO: SERVER_PORT=$SERVER_PORT"
+echo "INFO: PASSWORD=$PASSWORD"
+echo "INFO: METHOD=$METHOD"
+echo "INFO: TIMEOUT=$TIMEOUT"
+echo "INFO: DNS_ADDR=$DNS_ADDR"
+exec ss-server -a nobody --fast-open -u \
+              -s "$SERVER_ADDR" \
+              -p "$SERVER_PORT" \
+              -k "$PASSWORD"    \
+              -m "$METHOD"      \
+              -t "$TIMEOUT"     \
+              -d "$DNS_ADDR"    \
